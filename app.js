2fa7bd2926a03552c80157ae86a36affe9ed8d91
vero <veronicacanido@gmail.com>
2022-04-11 10:07:56 -0400

    some styles added, made descriptive list elements, using map to keep k-v pairs of id's and duplicated cuteness values :)

--- app.js
+++ app.js
@@ -4,33 +4,48 @@ const catProfiles = [
     id: 1,
     age: 2,
     isChonky: true,
+    image:
+      "https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png",
+    calculateCute: 100,
   },
   {
     name: "Lumi",
     id: 2,
     age: 2,
     isChonky: false,
+    image:
+      "https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png",
+    calculateCute: 50,
   },
   {
     name: "Coco",
     id: 3,
     age: 5,
     isChonky: false,
+    image:
+      "https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png",
+    calculateCute: 100,
   },
   {
     name: "Lumi2",
     id: 2,
     age: 2,
     isChonky: false,
+    image:
+      "https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png",
+    calculateCute: 50,
   },
 ];
 
 //eliminate dups
-const uniqueIds = new Set();
+const uniqueIds = new Map();
 const uniqueCatProfiles = catProfiles.filter((e) => {
   if (!uniqueIds.has(e.id)) {
-    uniqueIds.add(e.id);
+    uniqueIds.set(e.id, e.calculateCute);
     return true;
+  } else {
+    uniqueIds.set(e.id, uniqueIds.get(e.id) + e.calculateCute);
+    return false;
   }
 });
 console.log(uniqueCatProfiles);
@@ -38,12 +53,25 @@ console.log(uniqueCatProfiles);
 const getKittiesList = uniqueCatProfiles
   .map((e) => {
     console.log(e);
-    return `<li key=${e.id}>` + e.name + "</li>";
+    return `
+    <div class='profile-container'>
+    <img src=${e.image}>
+    <dt key=${e.id}> 
+      
+      <span class='cat-name-header'>${e.name}</span> 
+    </dt>
+    <dd>${e.name} is ${
+      e.isChonky ? "chonky" : "not chunk"
+    }. <br>Cuteness level: ${uniqueIds.get(e.id)}</dd>
+    </div>
+    `;
   })
   .join("");
 console.log(getKittiesList);
+
+//replace div#container in index.html with js rendered html
 document.getElementById("container").innerHTML = `
-    <ul>
+    <dl>
      ${getKittiesList}
-    </ul>
+    </dl>
 `;
